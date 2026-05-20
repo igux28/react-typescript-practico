b813ae3fcde765ab2060a8241a0e1a498819182a
Jonathan Jones <8566031+igux28@users.noreply.github.com>
2026-05-20 08:49:13 +0000

    useContesxt

--- mi-app/src/components/Language/Header.tsx
+++ mi-app/src/components/Language/Header.tsx
@@ -1,17 +1,17 @@
-import { useContext } from 'react'
-import { LangContext } from './SelectLang'
+import { useContext } from "react";
+import { LangContext } from "../../context/LangContext";
 
 export function Header() {
-  const t = useContext(LangContext)
+  const t = useContext(LangContext);
 
   if (!t) {
-    return <p>Error: falta LangProvider</p>
+    return <p>Error: falta LangProvider</p>;
   }
 
   return (
-    <header style={{ margin: '1rem 0', borderBottom: '1px solid #ccc' }}>
+    <header style={{ margin: "1rem 0", borderBottom: "1px solid #ccc" }}>
       <h1>{t.bienvenida}</h1>
       <p>{t.despedida}</p>
     </header>
-  )
+  );
 }
